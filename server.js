9ff38521c5a372b43b2f8d668ffa04360c626b05
joemay <joseluis@affenbits.com>
2014-04-29 17:39:54 -0500

    Add absolute path for static index file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create(),
 	fs = require('fs'),
-	path = 'index.html';
+	path = '/Applications/MAMP/htdocs/Cakekizer/index.html';
 
 
 function puts(error, stdout, stderr) {
@@ -8,33 +8,8 @@ function puts(error, stdout, stderr) {
 }
 
 page.open(path, function(status) {
-
 	var result = page.evaluate(function() {
-
-		// main.js
-		var client = new ZeroClipboard(document.getElementById("copy-button"), {
-			moviePath: "/vendors/ZeroClipboard.swf"
-		});
-
-
-		client.on('ready', function(readyEvent) {
-			console.log( 'ZeroClipboard SWF is ready!' );
-
-			client.on('aftercopy', function(event) {
-				// `this` === `client`
-				// `event.target` === the element that was clicked
-				event.target.style.display = 'none';
-				//alert('Copied text to clipboard: ' + event.data['text/plain']);
-			});
-		});
-
-
-
 		return cakekizer($('#container').html());
-
-
-
-
 	});
 	console.log(result);
 	phantom.exit();
